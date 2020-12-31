6429dca57ba4c58df0c518fab336f28590d39889
WesloTheWeb <wesleyadwebster@gmail.com>
2020-12-30 23:25:36 -0600

    initial set up

---
+++ src/components/BookList.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    {{title}}
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+
+  data() {
+    return {
+      title: "All Books",
+    };
+  },
+};
+</script>
+
+<style>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
